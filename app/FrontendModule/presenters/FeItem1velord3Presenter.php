cd566f9a98d5b334435dd0abcee2b91ee252d7dc
admin <admin>
2017-05-09 14:10:23 +0200

    - první form pro ověření příbuznosti

--- app/FrontendModule/presenters/FeItem1velord3Presenter.php
+++ app/FrontendModule/presenters/FeItem1velord3Presenter.php
@@ -2,6 +2,35 @@
 
 namespace App\FrontendModule\Presenters;
 
+use App\Forms\KinshipVerificationForm;
+
 class FeItem1velord3Presenter extends FrontendPresenter {
+
+	/** @var  KinshipVerificationForm */
+	private $kinshipVerificationForm;
+
+	public function __construct(KinshipVerificationForm $kinshipVerificationForm) {
+		$this->kinshipVerificationForm = $kinshipVerificationForm;
+	}
+
+	public function actionDefault() {
+
+
+	}
+
+	public function createComponentKinshipVerificationForm() {
+		$form = $this->kinshipVerificationForm->create();
+		$renderer = $form->getRenderer();
+		$renderer->wrappers['controls']['container'] = NULL;
+		$renderer->wrappers['pair']['container'] = 'div class=form-group';
+		$renderer->wrappers['pair']['.error'] = 'has-error';
+		$renderer->wrappers['control']['container'] = 'div class=col-md-6';
+		$renderer->wrappers['label']['container'] = 'div class="col-md-3 control-label"';
+		$renderer->wrappers['control']['description'] = 'span class=help-block';
+		$renderer->wrappers['control']['errorcontainer'] = 'span class=help-block';
+		$form->getElementPrototype()->class('form-horizontal');
+
+		return $form;
+	}
 
 }
