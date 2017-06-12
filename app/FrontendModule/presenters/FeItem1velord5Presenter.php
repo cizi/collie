9b8182a748e40ed425103022888d1d8e55f31046
admin <admin>
2017-06-12 14:09:51 +0200

    - FE je nyní v CHS vidět i plemeno (dle požadavku)

--- app/FrontendModule/presenters/FeItem1velord5Presenter.php
+++ app/FrontendModule/presenters/FeItem1velord5Presenter.php
@@ -3,6 +3,7 @@
 namespace App\FrontendModule\Presenters;
 
 use App\Enum\StateEnum;
+use App\Model\EnumerationRepository;
 use App\Model\UserRepository;
 
 class FeItem1velord5Presenter extends FrontendPresenter {
@@ -10,12 +11,18 @@ class FeItem1velord5Presenter extends FrontendPresenter {
 	/** @var UserRepository  */
 	private $userRepository;
 
-	public function __construct(UserRepository $userRepository) {
+	/** @var EnumerationRepository */
+	private $enumerationRepository;
+
+	public function __construct(UserRepository $userRepository, EnumerationRepository $enumerationRepository) {
 		$this->userRepository = $userRepository;
+		$this->enumerationRepository = $enumerationRepository;
 	}
 
 	public function actionDefault() {
 		$this->template->users = $this->userRepository->findCatteries();
 		$this->template->stateEnum = new StateEnum();
+		$this->template->lang = $this->langRepository->getCurrentLang($this->session);
+		$this->template->enumRepo = $this->enumerationRepository;
 	}
 }
